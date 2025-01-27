cb68c8927fbcc984852b0f39a0e2680a6fe0277a
ancopanco <anasaponjic1999@gmail.com>
2025-01-27 08:57:04 +0100

    Add sql scripts

---
+++ src/main/resources/sql_scripts/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO Team (name) VALUES ('Development');
+
+INSERT INTO Employee (id, name, idTeam, isTeamLead) VALUES 
+(123456, 'Mirko', 1, TRUE),
+(987654, 'Predrag', 1, FALSE),
+(654321, 'Petar', 1, FALSE),
+(321654, 'Vojislav', 1, FALSE);
